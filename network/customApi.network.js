5169d51890ec739ecc7b7ecadd2d8f6630ce6fdf
Théo HERVEUX <hurobaki@gmail.com>
2019-03-10 13:35:49 +0100

    move api url to const

--- network/customApi.network.js
+++ network/customApi.network.js
@@ -1,10 +1,12 @@
 const fetch = require('node-fetch');
 const { formatNetworkResponse } = require('./formatNetworkResponse');
 
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+
 const customApiNetwork = {
     customApi: () => ({
         get: async (options = null) => {
-            const response = await fetch('https://jsonplaceholder.typicode.com/todos', { options });
+            const response = await fetch(API_URL, { options });
             return ({ data, status } = await formatNetworkResponse(response));
         },
         set: 'SET from CustomApi network'
